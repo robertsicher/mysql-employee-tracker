a24c0fef9d99dcee28b8f83edccfe78be7bdcdaa
robertsicher <62100753+robertsicher@users.noreply.github.com>
2020-12-15 21:41:44 +0000

    Update sql-queries.js

--- sql-queries.js
+++ sql-queries.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Doozle95@",
+    password: "",
     database: "employeetracker"
 });
 
